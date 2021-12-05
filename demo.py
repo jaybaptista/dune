fix HQ units

demo.py
@@ -28,21 +28,30 @@ st.session_state.type = st.selectbox('DM Profile', ('NFW', 'Hernquist'))
 st.session_state.N = st.slider(
     'Number of Stars', min_value=10, max_value=1000, value=None, step=5)
 st.session_state.rp = st.slider(
-    'Plummer Radius [kpc]', min_value=.1, max_value=5., value=None, step=.1)
+    'Plummer Radius [kpc] (NFW only)', min_value=.1, max_value=5., value=None, step=.1)
 st.session_state.a = st.slider(
-    'Scale Radius [kpc]', min_value=.1, max_value=5., value=None, step=.1)
+    'Scale Radius (kpc in NFW, pc in Hernquist)', min_value=.1, max_value=50., value=None, step=.1)
 st.session_state.mass = st.slider(
     'Mass [Msun]', min_value=100., max_value=1000., value=None, step=100.)
 
 rho = (6.4e7 * u.M_sun / (u.kpc**3))
 
 data = None
+potential = None
+force = None
 
 if st.session_state.type == 'NFW':
-    data = mock.GeneratePlummerNFW(st.session_state.N, st.session_state.rp *
+    mock_output = mock.GeneratePlummerNFW(st.session_state.N, st.session_state.rp *
                                    u.kpc, st.session_state.a * u.kpc, rho, st.session_state.mass * u.M_sun)
+    data = mock_output[0]
+    potential = mock_output[1]
+    force = mock_output[2]
+
 elif st.session_state.type == 'Hernquist':
-    data = mock.GenerateHernquist(st.session_state.N, st.session_state.a * u.kpc, st.session_state.mass * u.M_sun)
+    mock_output = mock.GenerateHernquist(st.session_state.N, st.session_state.a * u.pc, st.session_state.mass * u.M_sun)
+    data = mock_output[0]
+    potential = mock_output[1]
+    force = mock_output[2]
 
 
 df = pd.DataFrame(data)
@@ -70,8 +79,13 @@ axs[1,0].hist(df.vt, histtype='step', color='k')
 axs[2,0].hist(df.vr, histtype='step', color='k')
 
 axs[0,1].scatter(df.r, df.vd, c='k', alpha=.2)
-axs[1,1].scatter(df.r, df.vt, c='k', alpha=.2)
-axs[2,1].scatter(df.r, df.vr, c='k', alpha=.2)
+
+axs[1,1].scatter(data['r'], force(data['r']))
+axs[2,1].scatter(data['r'], potential(data['r']))
+
+axs[0,1].set_xscale('log')
+axs[1,1].set_xscale('log')
+axs[2,1].set_xscale('log')
 
 axs[0,1].set_xlabel('r [kpc]')
 axs[1,1].set_xlabel('r [kpc]')
@@ -82,8 +96,8 @@ axs[1,0].set_xlabel('$V_R$ [km/s]')
 axs[2,0].set_xlabel('$V_T$ [km/s]')
 
 axs[0,1].set_ylabel('$\\sigma$ [km/s]')
-axs[1,1].set_ylabel('$V_R$ [km/s]')
-axs[2,1].set_ylabel('$V_T$ [km/s]')
+axs[1,1].set_ylabel('Specific Force [$m/s^2$]')
+axs[2,1].set_ylabel('Specific Energy [$m^2/s^2$]')
 
 axs[0,0].axvline(np.mean(df.vd), c='r')
 
@@ -100,12 +114,11 @@ axs[2,0].axvline(np.mean(df.vt)+np.std(df.vt), c='k', alpha=.3, ls='--')
 axs[2,0].axvline(np.mean(df.vt)-np.std(df.vt), c='k', alpha=.3, ls='--')
 
 axs[0,0].text(.025, 1.05, 'Velocity dispersion', transform=axs[0,0].transAxes)
-axs[0,1].text(.025, 1.05, 'Dispersion over radius', transform=axs[0,1].transAxes)
+axs[0,1].text(.025, 1.05, 'Radial Velocity', transform=axs[0,1].transAxes)
 axs[1,0].text(.025, 1.05, 'Radial velocity', transform=axs[1,0].transAxes)
-axs[1,1].text(.025, 1.05, 'Radial velocity over radius', transform=axs[1,1].transAxes)
+axs[1,1].text(.025, 1.05, 'Force Function', transform=axs[1,1].transAxes)
 axs[2,0].text(.025, 1.05, 'Tangential velocity', transform=axs[2,0].transAxes)
-axs[2,1].text(.025, 1.05, 'Tangential velocity over radius', transform=axs[2,1].transAxes)
+axs[2,1].text(.025, 1.05, 'Potential Function', transform=axs[2,1].transAxes)
 
 fig_analytics.tight_layout()
 st.pyplot(fig_analytics)
-

dune/mock.py
@@ -1,3 +1,4 @@
+from dune.profiles import nfw
 import numpy as np
 import astropy.constants as c
 import astropy.units as u
@@ -18,7 +19,7 @@ def GeneratePlummerNFW(N, rp, a, rho, mass):
     vels = VelocitySampler(nfw_profile.nfw_potential,
                            coords['r'], np.sqrt(sigma))
 
-    return {**coords, **coords_xyz, **vels}
+    return {**coords, **coords_xyz, **vels}, nfw_profile.nfw_potential, nfw_profile.nfw_force
 
 
 def GenerateHernquist(N, a, mass):
@@ -29,8 +30,7 @@ def GenerateHernquist(N, a, mass):
 
     # Get kinematics
     sigma = hq_profile.get_dispersion(coords['r'])
-    print(abs(sigma))
     vels = VelocitySampler(hq_profile.hernquist_potential,
                            coords['r'], np.sqrt(abs(sigma)))
 
-    return {**coords, **coords_xyz, **vels}
+    return {**coords, **coords_xyz, **vels}, hq_profile.hernquist_potential, hq_profile.hernquist_force

dune/profiles/hernquist.py
@@ -95,7 +95,7 @@ class Hernquist():
             spherical coordinates for the generated points
         '''
         return {
-        'r': self.generate_radii(N), 'theta': self.generate_theta(N), 'phi':self.generate_phi(N)
+        'r': self.generate_radii(N).to(u.kpc), 'theta': self.generate_theta(N), 'phi':self.generate_phi(N)
         }
 
     def convert_to_cartesian(self, coords):
@@ -176,7 +176,7 @@ class Hernquist():
             raise ValueError('value must be specified in astropy units')
 
 
-        pot = -1*c.G * self.enclosed_mass(r) / (r+a)
+        pot = -1*c.G * self.mass / (r+a)
 
         return pot
 
@@ -209,7 +209,7 @@ class Hernquist():
 
         force = -1*c.G * self.mass * r * (r-(2*a)) / (r+a)**4
 
-        return force
+        return force.decompose()
 
     def get_dispersion(self, r, a=None, dyn_mass=None):
         '''
@@ -253,8 +253,8 @@ class Hernquist():
             _sum = [quad(integrand, r_i, np.inf)[0] for r_i in r.value]
             output = ((1/(self.hq_density(r).decompose()))
                       * _sum * self.integrated_units)
-            return output
+            return output.to(u.km**2/u.s**2)
         else:
             output = ((1/(self.hq_density(r)).decompose()) * quad(integrand,
                       r.value, np.inf) * self.integrated_units)
-            return output[0]
+            return output[0].to(u.km**2/u.s**2)